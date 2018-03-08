eafa09c56ca471f5c3acd6538289d845e7d99e1d
Rima Jean <rima.jean@gmail.com>
2018-03-08 22:11:43 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,13 @@
 class CashRegister
   attr_accessor :total, :discount
 
+  def initialize(employee_discount = 0)
+    @total = 0
+    @discount = employee_discount
+    @items = []
+    @prices = []
+  end
+
 
 
 
